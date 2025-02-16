56131b1223a5e790f226fb64ea8ea30288023fc3
Eric Chang <a1756972831@gmail.com>
2025-02-16 14:42:55 +0800

    feat: 標籤組件化

---
+++ resources/views/components/lable-item.blade.php
@@ -0,0 +1,2 @@
+@props(['label', 'color' => 'primary'])
+<div class="badge badge-{{ $color }}">{{ $label }}</div>
---
+++ resources/views/components/lable-list.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-col gap-2 p-2">
+    <h3 class="font-bold mb-3">熱門標籤</h3>
+    <div class="flex flex-wrap gap-2">
+        <x-lable-item color="primary" label="Laravel" />
+        <x-lable-item color="secondary" label="PHP" />
+        <x-lable-item color="accent" label="Web開發" />
+        <x-lable-item color="ghost" label="JavaScript" />
+        <x-lable-item color="ghost" label="Vue.js" />
+        <x-lable-item color="ghost" label="React" />
+    </div>
+</div>
