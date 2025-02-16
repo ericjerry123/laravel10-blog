67924ef3d67a5aebf00baa2d18bac62d8e8fe44f
Eric Chang <a1756972831@gmail.com>
2025-02-16 16:12:26 +0800

    feat: 新增登入功能

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreLoginRequest;
 use App\Http\Requests\StoreRegisterRequest;
 use App\Services\AuthService;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,6 +27,20 @@ class AuthController extends Controller
         $data = $request->validated();
 
         $this->authService->register($data);
+
+        return redirect()->route('posts.index');
+    }
+
+    public function loginView()
+    {
+        return view('auth.login');
+    }
+
+    public function login(StoreLoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $this->authService->login($data);
 
         return redirect()->route('posts.index');
     }
--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -3,11 +3,17 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthRepository
 {
     public function register(array $data)
     {
         return User::create($data);
+    }
+
+    public function login(array $data)
+    {
+        return Auth::attempt($data);
     }
 }
